42c112bb7fe82c1aaad88c44b30c747fc3cfd2ca
dummydhbw <dummydhbw@gmail.com>
2024-10-10 09:48:00 +0000

    MyHomePage ist nun ein stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
